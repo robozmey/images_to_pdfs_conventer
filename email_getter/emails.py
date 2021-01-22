6b68de933499f06e1636d574b6f80577fc5c3178
robozmey <vova@kpnn.ru>
2021-01-22 05:47:26 +0300

    changed_getter

--- email_getter/emails.py
+++ email_getter/emails.py
@@ -13,22 +13,21 @@ for line in [line1.split() for line1 in all_fios_and_email]:
 
 #  + ' ' + x[1] + ' ' + x[2] + ' ' + x[3]
 
-list = list(dict.fromkeys([x[0] + ' ' + x[1] + ' ' + x[2] + ' ' + x[3] for x in sorted(list(filter(lambda x: fios.count(x[1] + ' ' + x[2] + ' ' + x[3]) > 0, list2)), key = lambda x: (x[1], x[2], x[3]))]))
+list2 = list(dict.fromkeys([x[0] + ' ' + x[1] + ' ' + x[2] + ' ' + x[3] for x in sorted(list(filter(lambda x: fios.count(x[1] + ' ' + x[2] + ' ' + x[3]) > 0, list2)), key = lambda x: (x[1], x[2], x[3]))]))
 
 
 file_tour_emails = open('участники_тура_почты.txt', encoding='utf-8', mode='w')
 
-file_tour_emails.write('\n'.join(list))
+file_tour_emails.write('\n'.join(list2))
 
 file_tour_emails.close()
 
-list = list(dict.fromkeys([x[0] for x in sorted(list(filter(lambda x: fios.count(x[1] + ' ' + x[2] + ' ' + x[3]) > 0, list2)), key = lambda x: (x[1], x[2], x[3]))]))
-
+list1 = list(dict.fromkeys([x[0] for x in sorted(list(filter(lambda x: fios.count(x[1] + ' ' + x[2] + ' ' + x[3]) > 0, list2)), key = lambda x: (x[1], x[2], x[3]))]))
 
 file_tour_emails = open('участники_тура_почты_just.txt', encoding='utf-8', mode='w')
 
-file_tour_emails.write('\n'.join(list))
+file_tour_emails.write('\n'.join(list1))
 
 file_tour_emails.close()
 
-print('\n'.join(list))
+print('\n'.join(list2))
